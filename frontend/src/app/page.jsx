9ab76cb98fffe1e5b7f6fde185a5d217ad519bb6
AditaySingh06 <aadiadityasingh2003@gmail.com>
2025-03-21 20:43:28 +0530

    spotlight

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,42 +1,26 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Link from "next/link";
-import {
-  Image,
-  Code,
-  Crop,
-  Filter,
-  Layers,
-  Zap,
-  Download,
-  Github,
-  ArrowRight,
-  Check,
-} from "lucide-react";
+import { Github, ArrowRight } from "lucide-react";
 import Navbar from "../../components/Navbar";
 import { FeaturesSectionDemo } from "../../components/FeatureSectionDemo";
 import { HeroSectionOne } from "../../components/Hero-Section";
-import { CodeBlockDemo, CodeBlockFive, CodeBlockFour, CodeBlockThree, CodeBlockTwo } from "../../components/CodeBlockDemo";
 import Example from "../../components/Example";
 import Installation from "../../components/Installation";
+import { Spotlight } from "../../components/ui/spotlight-new";
 
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex w-full min-h-screen flex-col">
       {/* Header */}
-      <div className="mb-20 ">
+      <div className=" mb-20 ">
         <Navbar />
       </div>
-      <div className="sidebar">
-       
-      </div>
+      <div className="sidebar"></div>
       <main className="flex-1">
         <section id="hero-section mt-20">
+          <Spotlight/>
           <HeroSectionOne />
         </section>
-        
 
         {/* Features Section */}
         <section
@@ -60,12 +44,10 @@ export default function Home() {
         </section>
 
         {/* Examples Section */}
-        <Example/>
+        <Example />
 
         {/* Installation Section */}
-        <Installation/>
-
-        
+        <Installation />
 
         {/* CTA Section */}
         <section className="w-full py-12 md:py-24 lg:py-32 border-t">
@@ -80,18 +62,18 @@ export default function Home() {
               </p>
             </div>
             <div className="mx-auto flex flex-col gap-2 min-[400px]:flex-row">
-            <button className="w-50 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-            <div className="flex items-center justify-center gap-2 text-md">
-              Get Started
-              <ArrowRight />
-            </div>
-          </button>
-          <button className="w-50 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
-            <div className="flex items-center justify-center gap-2 text-md">
-              View on GitHub
-              <Github />
-            </div>
-          </button>
+              <button className="w-50 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+                <div className="flex items-center justify-center gap-2 text-md">
+                  Get Started
+                  <ArrowRight />
+                </div>
+              </button>
+              <button className="w-50 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
+                <div className="flex items-center justify-center gap-2 text-md">
+                  View on GitHub
+                  <Github />
+                </div>
+              </button>
             </div>
           </div>
         </section>
