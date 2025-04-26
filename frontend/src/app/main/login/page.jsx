931c4415de753d0443cfae816ee8413b81f2ab50
AditaySingh06 <aadiadityasingh2003@gmail.com>
2025-04-26 10:32:59 +0530

    lib-publish-code

--- frontend/src/app/main/login/page.jsx
+++ frontend/src/app/main/login/page.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import Link from "next/link";
 import { ArrowRight, Github, Image } from "lucide-react";
 import { Button } from "@/components/ui/button";
